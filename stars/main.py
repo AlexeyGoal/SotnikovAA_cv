94cf80260815f35a6bb84ff5a543802708267cfd
AlexeyGoal <sotnikovalexei844@gmail.com>
2025-03-29 22:18:25 +0800

    Update main.py

--- stars/main.py
+++ stars/main.py
@@ -1,1 +1,17 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from skimage.measure import label,regionprops
 
+image = np.load("stars.npy")
+
+
+labeled = label(image)
+
+reg = regionprops(labeled)
+
+result = 0
+for i in reg:
+    if i.solidity < 1:
+        result+=1
+
+print(result)
